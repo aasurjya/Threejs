1aab50e977a131dfa6241c48f7e57fa9500fc8eb
Aasurjya <44926849+aasurjya@users.noreply.github.com>
2025-06-02 15:23:04 +0530

    Update model interaction components

--- src/components/RenderModel.jsx
+++ src/components/RenderModel.jsx
@@ -10,18 +10,22 @@ const RenderModel = ({ children, className }) => {
       className={clsx("w-screen h-screen -z-10 relative", className)}
       shadows={false}
       dpr={[1, 2]}
-      camera={{ position: [0, 0, 5], fov: 75 }}
+      camera={{
+        position: [0, 0, 5],
+        fov: 45,
+      }}
     >
       <Suspense fallback={null}>{children}</Suspense>
-      <OrbitControls 
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[5, 5, 5]} intensity={1} />
+      <OrbitControls
+        enableZoom={true}
+        enablePan={true}
         enableRotate={true}
-        enablePan={false}
-        enableZoom={false}
+        minDistance={3}
+        maxDistance={10}
         rotateSpeed={0.5}
-        minPolarAngle={Math.PI / 4}
-        maxPolarAngle={Math.PI / 1.5}
-        minAzimuthAngle={-Math.PI / 4}
-        maxAzimuthAngle={Math.PI / 4}
+        target={[0, 0, 0]}
       />
       <Environment preset="dawn" />
     </Canvas>
